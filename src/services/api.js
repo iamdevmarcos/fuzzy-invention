e026558aab122746e750e2ffb286777c260a7c29
Felipe Alves <profelipealves@outlook.com>
2024-03-01 23:07:22 -0300

    refactor: removing unusable call

--- src/services/api.js
+++ src/services/api.js
@@ -9,15 +9,3 @@ export async function getFilms() {
     console.error(e);
   }
 }
-
-export async function getFilmDetails(id) {
-  const url = `https://api.themoviedb.org/3/movie/${id}?language=pt-br`;
-
-  try {
-    const response = await fetch(url);
-    const result = await response.json();
-    return result;
-  } catch (e) {
-    console.error(e);
-  }
-}
